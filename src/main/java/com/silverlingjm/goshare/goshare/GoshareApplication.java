9bf9eea62ee29930c0d0212c0405c99b8bbfd695
Rowan Atkinson <rowaneatkinson@gmail.com>
2020-12-10 20:02:59 -0500

    first commit

---
+++ src/main/java/com/silverlingjm/goshare/goshare/GoshareApplication.java
@@ -0,0 +1,13 @@
+package com.silverlingjm.goshare.goshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silverlingjm/goshare/goshare/GoshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverlingjm.goshare.goshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
