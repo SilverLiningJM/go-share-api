ff428eeed8abce901be0c6ce897c2bd2e0ab42f4
Rowan Atkinson <rowaneatkinson@gmail.com>
2020-12-20 23:36:34 -0500

    implemented user service for basic CRUD

--- src/main/java/com/silverlingjm/goshare/repositories/IUserRepository.java
+++ src/main/java/com/silverlingjm/goshare/repositories/IUserRepository.java
@@ -2,6 +2,8 @@ package com.silverlingjm.goshare.repositories;
 
 import com.silverlingjm.goshare.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("pgUser")
 public interface IUserRepository extends CrudRepository<User, Long> {
 }
--- src/main/java/com/silverlingjm/goshare/services/UserService.java
+++ src/main/java/com/silverlingjm/goshare/services/UserService.java
@@ -2,16 +2,65 @@ package com.silverlingjm.goshare.services;
 
 import com.silverlingjm.goshare.models.User;
 import com.silverlingjm.goshare.repositories.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private IUserRepository userRepository;
 
-    public Iterable<User> list() {
+    public User save(User user) {
+        logger.info("Attempting to add new user");
+        User newUser = this.userRepository.save(user);
+        logger.info("New user successfully added with id: " + newUser.getId());
+        return newUser;
+    }
+
+    public Optional<User> findById(Long id) {
+        logger.info("attempting to fetch user " + id);
+        Optional<User> user = this.userRepository.findById(id);
+        user.ifPresentOrElse(
+                u -> logger.info("User is present in system"),
+                () -> logger.error("User " + id + " does not exist"));
+        return user;
+    }
+
+    public Iterable<User> finaAll() {
+        logger.info("attempting to fetch all users");
         return userRepository.findAll();
     }
+
+    public boolean deleteById(Long id) {
+        try {
+            logger.info("Attempting to delete user with id: " + id);
+            this.userRepository.deleteById(id);
+            logger.info("Successfully deleted user with id: " + id);
+            return true;
+        } catch(Exception e) {
+            logger.error("unable to delete user with id: " + id + ". Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean updateById(Long id, User user) {
+        try {
+            user.setId(id);
+            logger.info("updating user " + id);
+            this.userRepository.save(user);
+            logger.info("Successfully updated user " + id);
+            return true;
+        } catch(Exception e) {
+            logger.error("Error updating user " + id + ": " + e.getMessage());
+        }
+        return false;
+    }
+
 }
